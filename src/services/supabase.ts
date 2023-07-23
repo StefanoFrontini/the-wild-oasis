1bf411051017788223a979c28063551e798eef4b
Stefano Frontini <stefanofrontini75@gmail.com>
2023-07-23 18:39:57 +0200

    eslint configuration

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://miykajiatnodhadwtghn.supabase.co";
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
