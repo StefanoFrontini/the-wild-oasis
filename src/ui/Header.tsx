afd03b557b885c29393257ab24ab435e55c06fb1
Stefano Frontini <stefanofrontini75@gmail.com>
2023-07-23 18:23:14 +0200

    initial commit

---
+++ src/ui/Header.tsx
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+const StyleHeader = styled.div`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+function Header() {
+  return <StyleHeader>Header</StyleHeader>;
+}
+
+export default Header;
